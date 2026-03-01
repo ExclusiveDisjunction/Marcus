60a68b6d84bbadd543c1e5cf1bcf17461e165589
exdisj <exdisj@outlook.com>
2026-03-01 11:12:16 -0500

    Initial Commit

---
+++ Marcus/MarcusApp.swift
@@ -0,0 +1,21 @@
+//
+//  MarcusApp.swift
+//  Marcus
+//
+//  Created by Hollan Sellars on 3/1/26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct MarcusApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ MarcusTests/MarcusTests.swift
@@ -0,0 +1,16 @@
+//
+//  MarcusTests.swift
+//  MarcusTests
+//
+//  Created by Hollan Sellars on 3/1/26.
+//
+
+import Testing
+
+struct MarcusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
